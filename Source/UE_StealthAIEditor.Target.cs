f1642973dca165eb145bd6e5ddab4a3c4a33b4ae
B-JoCharm <121530622+B-JoCharm@users.noreply.github.com>
2026-05-29 20:16:32 +0900

    Initial commit

---
+++ Source/UE_StealthAIEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class UE_StealthAIEditorTarget : TargetRules
+{
+	public UE_StealthAIEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		ExtraModuleNames.Add("UE_StealthAI");
+	}
+}
